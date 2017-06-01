d94b5df94b5aaa77c4dfb62033991725ffae4001
Josh Dick <josh@joshdick.net>
2017-06-01 18:34:33 -0400

    Tweak terminal color scheme selection colors to be more sensible.
    Fixes #104.

--- build/build.js
+++ build/build.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const _ = require('lodash') // ships with termcolors; only used because of termcolors customization
 const doT = require('doT')
 const termcolors = require('termcolors')
 const { readFileSync, writeFileSync } = require('fs')
@@ -115,9 +116,45 @@ try {
 	const xresources = readFileSync(resolve(__dirname, '../term/One Dark.Xresources'), 'utf8')
 	const terminalPalette = termcolors.xresources.import(xresources)
 
+	let itermTemplate, terminalAppTemplate
+
+	// Compile custom terminal color templates based on ones that ship with termcolors
 	try {
-		writeFileSync(resolve(__dirname, '../term/One\ Dark.itermcolors'), termcolors.iterm.export(terminalPalette))
-		writeFileSync(resolve(__dirname, '../term/One\ Dark.terminal'), termcolors.terminalapp.export(terminalPalette))
+		itermTemplate = termcolors.export(
+			// From termcolors/lib/formats/iterm.js
+			readFileSync(resolve(__dirname, 'templates/One\ Dark.itermcolors')),
+			_.partialRight(_.mapValues, function (color) {
+				return color.toAvgRgbArray()
+			})
+		)
+
+		// From termcolors/lib/formats/terminal-app.js
+		const code = [
+			new Buffer('62706c6973743030d40102030405061516582476657273696f6e58246f626a65637473592461726368697665725424746f7012000186a0a307080f55246e756c6cd3090a0b0c0d0e554e535247425c4e53436f6c6f7253706163655624636c6173734f1027', 'hex'),
+			new Buffer('0010018002d2101112135a24636c6173736e616d655824636c6173736573574e53436f6c6f72a21214584e534f626a6563745f100f4e534b657965644172636869766572d1171854726f6f74800108111a232d32373b41484e5b628c8e9095a0a9b1b4bdcfd2d700000000000001010000000000000019000000000000000000000000000000d9', 'hex')
+		]
+
+		terminalAppTemplate = termcolors.export(
+			readFileSync(resolve(__dirname, 'templates/One\ Dark.terminal')),
+			// From termcolors/lib/formats/terminal-app.js
+			_.partialRight(_.mapValues, function (color) {
+				var srgb = color.toAvgRgbArray()
+				srgb = srgb.map(function (n) {
+					return n.toFixed(10).toString()
+				}).join(' ')
+				var output = code[0].toString('binary') + srgb + code[1].toString('binary')
+				output = (new Buffer(output, 'binary')).toString('base64')
+				return output.match(/.{1,68}/g).join('\n\t')
+			})
+		)
+
+	} catch (e) {
+		handleError('Error compiling terminal color template', e)
+	}
+
+	try {
+		writeFileSync(resolve(__dirname, '../term/One\ Dark.itermcolors'), itermTemplate(terminalPalette))
+		writeFileSync(resolve(__dirname, '../term/One\ Dark.terminal'), terminalAppTemplate(terminalPalette))
 	} catch (e) {
 		handleError('Error writing terminal color file', e)
 	}
